ba9bcf59b27fa9ba35941a13ef7ef99cf0b6e5b1
Jorge Meireles <jorgentoniomeireles@gmail.com>
2022-03-01 02:09:03 -0300

    Criação do construtor com dois atributos

--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,7 +1,7 @@
 package br.com.dio.desafio.dominio;
 
 public abstract class Conteudo {
-     protected  final  static  double XP_PADRAO = 10d;
+    protected  final  static  double XP_PADRAO = 10d;
 
     private  String titulo;
     private String descricao;
